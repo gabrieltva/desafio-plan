e9a4ceff7f24a57a32cd9711fec02341c7c46916
Gabriel Stafoca <gabriel@waba.com.br>
2024-08-02 23:19:36 -0300

    fix: menu mobile was fixed

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup>
 import { userDashboardRouterName } from '@/utils/user';
 import HeaderLink from './HeaderLink.vue';
 import { userToken, removeUserData } from "@/utils/user";
@@ -42,22 +42,25 @@ const toggleMobileMenu = () => {
             Sair
           </button>
           <button type="button" @click="toggleMobileMenu"
-            class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="mobile-menu-2" aria-expanded="false">
+            class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
             <span class="sr-only">Open main menu</span>
-            <svg v-if="showMenuMobile.value === false" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"></path>
-            </svg>
-            <svg v-else class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clip-rule="evenodd"></path>
-            </svg>
+            <span v-if="showMenuMobile === false">
+              <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"></path>
+              </svg>
+            </span>
+            <span v-else>
+              <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"></path>
+              </svg>
+            </span>
           </button>
         </div>
-        <div :class="`${showMenuMobile.value === true ? 'block': 'hidden'} justify-between items-center w-full lg:flex lg:w-auto lg:order-1`" id="mobile-menu-2">
+        <div :class="`${showMenuMobile === true ? 'block': 'hidden'} justify-between items-center w-full lg:flex lg:w-auto lg:order-1`" id="mobile-menu-2">
           <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
             <header-link :to="userDashboardRouterName()" :actived="true">Home</header-link>
             <header-link v-if="userRole() === 'admin'" to="adminStudentsList">Colaboradores</header-link>
