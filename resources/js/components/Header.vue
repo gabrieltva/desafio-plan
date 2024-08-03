94afdbf2131ac873260cbd272a53dd8d80717521
Gabriel Stafoca <gabriel@waba.com.br>
2024-08-02 22:59:52 -0300

    fix: menu header

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -23,6 +23,10 @@ const logout = async () => {
   }
 }
 
+const toggleMobileMenu = () => {
+  showMenuMobile.value = !showMenuMobile.value
+}
+
 </script>
 
 <template>
@@ -37,7 +41,7 @@ const logout = async () => {
             class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
             Sair
           </button>
-          <button  type="button"
+          <button type="button" @click="toggleMobileMenu"
             class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="mobile-menu-2" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
@@ -46,7 +50,7 @@ const logout = async () => {
                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
                 clip-rule="evenodd"></path>
             </svg>
-            <svg v-if="showMenuMobile.value === true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <svg v-else class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd"
                 d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                 clip-rule="evenodd"></path>
